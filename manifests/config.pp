bb89fb914bc0beb0678538ed5b1e174d1bef01a3
Thomas Cowling <tom.cowling@gmail.com>
2017-08-12 10:58:24 -0400

    Compose the config file some more
    - Closes #9 - config test after config change
    - Closes #10 - bug since there is no group attribute in the nginx conf

--- manifests/config.pp
+++ manifests/config.pp
@@ -28,12 +28,37 @@ class nginx::config (
     require => User[$user],
   }
 
+  exec { 'config_test':
+    path    => [ '/usr/local/bin' ],
+    command => "nginx -t -c ${base_directory}/nginx.conf",
+  }
+
   concat { 'nginx_config':
     ensure  => 'present',
     owner   => $user,
     group   => $group,
     mode    => '0640',
     path    => "${base_directory}/nginx.conf",
+    ensure_newline => true,
     require => File[$base_directory],
+    notify  => Exec['config_test'],
+  }
+
+  concat::fragment { 'nginx_config_header':
+    target  => 'nginx_config',
+    content => template('nginx/config/header.erb'),
+    order   => '00',
+  }
+
+  concat::fragment { 'nginx_config_include_basics':
+    target  => 'nginx_config',
+    content => template('nginx/config/basic.erb'),
+    order   => '01',
+  }
+
+  concat::fragment { 'nginx_config_include_events':
+    target  => 'nginx_config',
+    content => 'events {}',
+    order   => '02',
   }
 }
--- manifests/events.pp
+++ manifests/events.pp
@@ -1,5 +1,11 @@
 class nginx::events (
   Integer $worker_connections = 1024,
-) {
+) inherits nginx::config  {
   notice('Configuring nginx events')
+
+  concat::fragment { 'nginx_events':
+    target  => 'nginx_conf',
+    content => 'events {}',
+    order   => '01',
+  }
 }
