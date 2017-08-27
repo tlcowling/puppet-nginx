402816e2840f2daf02e0fb1846642303527e24be
Thomas Cowling <tom.cowling@gmail.com>
2017-08-27 14:23:47 +0100

    Use scope lookup to get the include dir for the snippets, also Closes #58

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,6 +2,6 @@ class nginx::params{
   $user = 'nginx'
   $group = 'nginx'
   $mode = '0750'
-  $base_directory = '/etc/nginx/'
-  $includes_directory = 'conf.d/'
+  $base_directory = '/etc/nginx'
+  $includes_directory = 'conf.d'
 }
